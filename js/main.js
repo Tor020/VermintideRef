39b1a8d285c2d79e2772c6f026b5b12854d9823b
Kevin <Tor000020@gmail.com>
2017-05-26 21:57:08 -0400

    c
    c

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+const iBtn = document.querySelector('.moreinfbtn')
+const iDiv = document.querySelector('.moreinfdiv')
 
+
+
+iBtn.addEventListener('click', () =>
+
+    iDiv.classList.toggle('hidden'));
